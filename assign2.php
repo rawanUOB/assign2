b0e1699f584c153298902d04cb329b7d96374b0e
rawan202102421 <rawan3almahoozi@gmail.com>
2024-11-24 15:57:33 +0300

    step 1

--- assign2.php
+++ assign2.php
@@ -1,3 +1,18 @@
 <?php
 
+#Defining the API
+
+$URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"; 
+
+#Fetching the data 
+
+$response = file_get_contents($URL);
+$result = json_decode($response, true);
+
+if (!$result || !isset($result["results"]) ) {
+    die('an error occured while fetching the data from the API! ');
+}
+
+$data = $result["results"]; 
+
 ?>
